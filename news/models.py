c6b488164d0368090d868a0cfac8efe471213c77
Vladislav Galimov <v.galimov@deliveryweb.ru>
2019-05-26 01:32:46 +0300

    change address field type

--- news/models.py
+++ news/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=256)
     text = models.TextField()
-    address = models.CharField(max_length=256, blank=True)
+    address = models.TextField(blank=True)
     news_id = models.IntegerField()
     is_new = models.BooleanField(default=False)
