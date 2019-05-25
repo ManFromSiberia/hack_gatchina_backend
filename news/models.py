05010364f65b80491976d2dae00407256da5b3de
Vladislav Galimov <v.galimov@deliveryweb.ru>
2019-05-25 21:08:59 +0300

    add model News

---
+++ news/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=256)
+    text = models.TextField()
+    address = models.CharField(max_length=256, blank=True)
