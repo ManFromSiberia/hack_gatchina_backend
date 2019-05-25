bbc270775f8cf7e3a626a04d1202f5bdc30bce0b
Vladislav Galimov <v.galimov@deliveryweb.ru>
2019-05-26 00:26:36 +0300

    add address extractor

--- parser/parser.py
+++ parser/parser.py
@@ -2,6 +2,8 @@ import re
 
 import requests
 from bs4 import BeautifulSoup
+from natasha import AddressExtractor
+from natasha.markup import show_markup, format_json
 
 
 class Parser:
@@ -14,12 +16,13 @@ class Parser:
         response = requests.get(url)
         return response.text
 
-    def __get_news_data(self, html):
+    def __get_news_data(self, html, news_id):
         soup = BeautifulSoup(html, 'lxml')
         content = soup.find('div', class_='inCenter')
         title = content.find('h1', class_='padd_b20').text
         text = content.find('div', class_='F17').text
-        return title, text
+        adresses = self.__get_address(text)
+        return {'title': title, 'text': text, 'news_id': news_id, 'addresses': adresses}
 
     def __get_last_link(self, html):
         soup = BeautifulSoup(html, 'lxml')
@@ -40,9 +43,44 @@ class Parser:
 
     def get_last_news(self):
         link = self.__get_last_link(self.html)
-        return self.__get_news_data(self.__get_html(link))
+        return self.__get_news_data(self.__get_html(link), self.__get_news_id(link))
 
     def get_all_news(self):
         links = self.__get_all_links(self.html)
         for link in links:
-            yield self.__get_news_data(self.__get_html(link))
+            yield self.__get_news_data(self.__get_html(link), self.__get_news_id(link))
+
+    def __get_news_id(self, link):
+        return link.split('=')[1]
+
+    def __get_address(self, text):
+        results = []
+        number = re.compile('^\d+\w?$')
+        word = re.compile('^[а-яА-Я]+')
+        raw_text = text.split('ул. ')[1:]
+        for text in raw_text:
+            addresses = text.split(',')
+            street = ''
+            for part in addresses:
+                part = part.strip()
+                if re.findall(word, part):
+                    street = part.split(' ')[0]
+                    part = part.replace('д. ', 'д.')
+                    results.append('ул. ' + part)
+                elif re.findall(number, part):
+                    results.append('ул. ' + street + ' д.' + part)
+                elif '-' in part:
+                    digits = part.split('-')
+                    for i in range(int(digits[0]), int(digits[1]) + 1):
+                        results.append('ул. ' + street + ' д.' + str(i))
+        return results
+
+    def test_nlp(self, text):
+        lines = text.splitlines()
+        extractor = AddressExtractor()
+        for line in lines:
+            matches = extractor(line)
+            spans = [_.span for _ in matches]
+            facts = [_.fact.as_json for _ in matches]
+            show_markup(line, spans)
+            print(format_json(facts))
