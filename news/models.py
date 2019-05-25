c78729a628acb223638f4c54bf4b8ea9f6a49f08
Vladislav Galimov <v.galimov@deliveryweb.ru>
2019-05-26 00:27:14 +0300

    add field news_id

--- news/models.py
+++ news/models.py
@@ -5,4 +5,5 @@ class News(models.Model):
     title = models.CharField(max_length=256)
     text = models.TextField()
     address = models.CharField(max_length=256, blank=True)
+    news_id = models.IntegerField()
     is_new = models.BooleanField(default=False)
