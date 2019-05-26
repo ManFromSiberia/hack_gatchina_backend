d67b9a2c282ac4206af5832b742137fd684196dd
manfromsiberia <manfromsiberia@yandex.ru>
2019-05-26 15:02:43 +0300

    add endpoint for complete news in news app

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from news.views import NewsUpdateView
+from news.views import NewsUpdateView, NewsPostCompleteView
 
 urlpatterns = [
     path('updates/', NewsUpdateView.as_view()),
+    path('<id>/complete/', NewsPostCompleteView.as_view())
 ]
