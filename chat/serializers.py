c553edd31bc8da1024bf3bceb07b268e98728ca0
manfromsiberia <manfromsiberia@yandex.ru>
2019-05-26 09:03:17 +0300

    rename app_chat_identification field in chat serializer

--- chat/serializers.py
+++ chat/serializers.py
@@ -7,4 +7,4 @@ class ChatDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chat
         fields = ('id', 'social_network', 'city', 'city_district', 'postal_code', 'street', 'house_number',
-                  'residential_complex', 'app_chat_identification', 'is_private_house')
+                  'residential_complex', 'chat_invite_link', 'is_private_house')
