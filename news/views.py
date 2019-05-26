add87549130c345f341831a714887197a5e5585d
Vladislav Galimov <v.galimov@deliveryweb.ru>
2019-05-26 08:30:27 +0300

    fix endpoint /news/updates/

--- news/views.py
+++ news/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from chat.models import Chat
-from chat.views import get_address_from_coordinates
 from news.models import News
 
 
@@ -13,18 +12,34 @@ class NewsUpdateView(APIView):
         new_news = News.objects.filter(is_new=True)
         for news in new_news:
             links = []
-            if news['address']:
-                for address in new_news['address']:
-                    geocode_yandex = geocoder.yandex(
-                        address,
-                        method='reverse',
-                        kind='house',
-                        lang='RU',
-                        key='7bb41e4d-5a27-4b8c-a856-ee56d3284019'
-                    )
-                    lat = geocode_yandex.lat
-                    lng = geocode_yandex.lng
-                    adrs = get_address_from_coordinates([lat, lng])
+            if news.address:
+                for address in news.address:
+                    print(address)
+                    if 'ул' in address:
+                        try:
+                            geocode_yandex = geocoder.yandex(
+                                address,
+                                method='reverse',
+                                kind='house',
+                                lang='RU',
+                                key='7bb41e4d-5a27-4b8c-a856-ee56d3284019'
+                            )
+                            lat = float(geocode_yandex.lat)
+                            lng = float(geocode_yandex.lng)
+                            adrs = get_address_from_coordinates([lat, lng], False)
+                        except ValueError:
+                            continue
+                    else:
+                        geocode_yandex = geocoder.yandex(
+                            address,
+                            method='reverse',
+                            kind='locality',
+                            lang='RU',
+                            key='7bb41e4d-5a27-4b8c-a856-ee56d3284019'
+                        )
+                        lat = float(geocode_yandex.lat)
+                        lng = float(geocode_yandex.lng)
+                        adrs = get_address_from_coordinates([lat, lng], True)
                     chats = Chat.objects.all()
                     try:
                         link = chats.filter(city=adrs.get('city'), street=adrs.get('street'),
@@ -35,9 +50,43 @@ class NewsUpdateView(APIView):
             if not links:
                 links = Chat.objects.all()
             result.append({
-                'title': news['title'],
-                'text': news['text'],
+                'title': news.title,
+                'text': news.text,
                 'chat_links': links,
             })
+            news.is_new = False
+            news.save()
 
         return Response(result)
+
+
+def get_address_from_coordinates(coordinates, is_city):
+    if is_city:
+        geocode_yandex = geocoder.yandex(
+            coordinates,
+            method='reverse',
+            kind='locality',
+            lang='RU',
+            key='7bb41e4d-5a27-4b8c-a856-ee56d3284019'
+        )
+        yandex_address_array = geocode_yandex.address.split(',')
+        locality = yandex_address_array[2]
+        street = ''
+        house = ''
+    else:
+        geocode_yandex = geocoder.yandex(
+            coordinates,
+            method='reverse',
+            kind='house',
+            lang='RU',
+            key='7bb41e4d-5a27-4b8c-a856-ee56d3284019'
+        )
+        yandex_address_array = geocode_yandex.address.split(',')
+        locality = yandex_address_array[2]
+        street = yandex_address_array[3]
+        house = yandex_address_array[4]
+    return {
+        'city': locality,
+        'street': street,
+        'house': house,
+    }
