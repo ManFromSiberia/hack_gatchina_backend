416fdf7b142d1b1c5834dfc4c7fe024dfb80ef9f
manfromsiberia <manfromsiberia@yandex.ru>
2019-05-26 09:03:37 +0300

    add create_channel action

--- chat/views.py
+++ chat/views.py
@@ -1,13 +1,17 @@
+import json
+
 from django.conf import settings
 from django.contrib.gis.geos import Point
+import requests
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from telethon import TelegramClient, sync, utils, events
+import asyncio
+from telethon import TelegramClient
 import geocoder
+from telethon.tl.functions.channels import CreateChannelRequest, InviteToChannelRequest, EditAdminRequest
+from telethon.tl.types import ChatAdminRights
 
-# [60.02833645805045, 30.250738203167884]
-# [59.9698057, 30.316310699999995]
 from chat.models import Chat
 from chat.serializers import ChatDetailSerializer
 
@@ -17,7 +21,11 @@ def get_chats_by_address(address, is_private_house=None):
     if is_private_house:
         result = chats.filter(postal_code=address.get('postal'))
     else:
-        result = chats.filter(street=address.get('street'), house_number=address.get('house'))
+        result = chats.filter(
+            city=address.get('city'),
+            street=address.get('street'),
+            house_number=address.get('house')
+        )
     return result
 
 
@@ -27,7 +35,7 @@ def get_address_from_coordinates(coordinates):
         method='reverse',
         kind='district',
         lang='RU',
-        key='7bb41e4d-5a27-4b8c-a856-ee56d3284019'
+        key=settings.YANDEX_MAP_KEY
     )
     yandex_address_array = geocode_yandex.address.split(',')
     district = ''
@@ -43,7 +51,7 @@ def get_address_from_coordinates(coordinates):
         method='reverse',
         kind='house',
         lang='RU',
-        key='7bb41e4d-5a27-4b8c-a856-ee56d3284019'
+        key=settings.YANDEX_MAP_KEY
     )
 
     yandex_address_array = geocode_yandex.address.split(',')
@@ -56,7 +64,7 @@ def get_address_from_coordinates(coordinates):
     geocode_mapquest = geocoder.mapquest(
         coordinates,
         method='reverse',
-        key='OXS9eKFyynkRwSMAaZDPMMJWcuKAZ9GP'
+        key=settings.MAPQUEST_MAP_KEY
     )
     postal = geocode_mapquest.postal
     return {
@@ -69,10 +77,38 @@ def get_address_from_coordinates(coordinates):
     }
 
 
-def create_chat(social_network):
-    if social_network == 0:
-        client = TelegramClient('allmemes_app', settings.TELEGRAM_API_ID, settings.TELEGRAM_API_HASH)
-        client.action()
+async def create_channel(chat_name):
+    client = TelegramClient('allmemes_app', settings.TELEGRAM_API_ID, settings.TELEGRAM_API_HASH)
+    # await client.start()
+    await client.connect()
+    channel = await client(CreateChannelRequest(
+        chat_name,
+        'Специально созданный чат для вас, сервисом Мой Двор',
+        broadcast=False,
+        megagroup=True
+    ))
+    goshan_bot = await client.get_input_entity('my_dvorbot')
+    await client(InviteToChannelRequest(
+        channel.chats[0].id,
+        [goshan_bot]
+    ))
+    rights = ChatAdminRights(
+        post_messages=True,
+        add_admins=True,
+        invite_users=True,
+        change_info=True,
+        ban_users=True,
+        delete_messages=True,
+        pin_messages=True,
+        edit_messages=True
+    )
+    await client(EditAdminRequest(channel.chats[0].id, goshan_bot, rights))
+    buff = await client.get_entity(channel.chats[0].id)
+    await client.disconnect()
+    response = requests.get(
+        f'https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/exportChatInviteLink?chat_id=-100{buff.id}'
+    )
+    return json.loads(response.text).get('result')
 
 
 class ChatsFromCoordinatesView(APIView):
@@ -99,10 +135,11 @@ class CreateNewChatView(APIView):
 
         if coordinates and social_network != -1:
             address = get_address_from_coordinates(coordinates)
-            chat_identification = create_chat(social_network)
 
-            print('Address', address)
             if not is_private_house:
+                chat_name = f"Чат {address.get('street')}, {address.get('house')}"
+                chat_identification = asyncio.new_event_loop().run_until_complete(create_channel(chat_name))
+
                 chat = Chat.objects.create(
                     social_network=social_network,
                     city=address.get('city'),
@@ -111,9 +148,12 @@ class CreateNewChatView(APIView):
                     street=address.get('street'),
                     house_number=address.get('house'),
                     coordinates=Point(coordinates),
-                    app_chat_identification='test_chat_identification'
+                    chat_invite_link=chat_identification
                 )
             else:
+                chat_name = f"Чат почтовый индекс {address.get('postal')}"
+                chat_identification = asyncio.new_event_loop().run_until_complete(create_channel(chat_name))
+
                 chat = Chat.objects.create(
                     social_network=social_network,
                     city=address.get('city'),
@@ -121,7 +161,7 @@ class CreateNewChatView(APIView):
                     postal_code=address.get('postal'),
                     coordinates=Point(coordinates),
                     is_private_house=is_private_house,
-                    app_chat_identification='test_chat_identification'
+                    chat_invite_link=chat_identification
                 )
             return Response(ChatDetailSerializer(chat).data)
         else:
